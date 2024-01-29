62ad4a1354d5fbe8f1cafa5d5abe2c60d756b9ee
Rune Harlyk <rune@harlyk.dk>
2024-01-29 15:16:54 +0100

    Speeds up get repos

--- profile.ps1
+++ profile.ps1
@@ -3,42 +3,20 @@ function Select-GitRepository {
         [string]$BasePath = ".",
         [int]$Depth = 1
     )
-
-    # $ignorePatterns = @('node_modules', 'cache') # Add more patterns as needed
-
-    # Get all directories up to the specified depth
     $allDirs = Get-ChildItem -Path $BasePath -Directory -Recurse -Depth $Depth
-
-    # Filter out ignored directories
-    $dirsToCheck = $allDirs #| Where-Object { $ignorePatterns -notcontains $_.Name }
     Write-Host "Checking $($allDirs.Count) directories..."
 
-    # Find repositories in parallel
-    $repositories = $dirsToCheck | ForEach-Object -Parallel {
+    $repositories = $allDirs | ForEach-Object -Parallel {
         $path = $_.FullName
-        # Change the current location to the directory being checked
         Push-Location -Path $path
-        try {
-            # Check if the current directory is inside a Git work tree
-            $isGitRepo = git rev-parse --is-inside-work-tree 2>$null
-            if ($isGitRepo -eq 'true') {
-                $lastWrite = Get-ChildItem -Path $path -Recurse -File | 
-                             Where-Object { 
-                                 $ignorePatterns -notcontains $_.Directory.Name
-                             } | 
-                             Sort-Object -Property LastWriteTime -Descending | 
-                             Select-Object -First 1
-
-                if ($lastWrite) {
-                    [PSCustomObject]@{
-                        Path = $path
-                        LastModified = $lastWrite.LastWriteTime
-                    }
-                }
+        $isGitRepo = git rev-parse --is-inside-work-tree 2>$null
+        if ($isGitRepo -eq 'true') {
+            [PSCustomObject]@{
+                Path = $path
+                LastModified = (Get-Item $path).LastWriteTime
             }
-        } finally {
-            Pop-Location
         }
+        Pop-Location
     } | Sort-Object LastModified -Descending | Select-Object -First 5
 
     # Display repositories
